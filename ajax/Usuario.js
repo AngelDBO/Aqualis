798aff7dfaedb144bd1a4ffd3b2924b59e275271
angel <angel@ACER-ANGEL>
2020-03-18 22:03:57 -0500

    Actualizacion

--- ajax/Usuario.js
+++ ajax/Usuario.js
@@ -1,24 +1,24 @@
 function ValidarUsuario() {
-        
+
     $.ajax({
-        data:$('#fomr_1').serialize(),
-        type:"POST",
-        url:'controllers/UsuarioController.php?opcion=validar_usuario',
-        beforeSend:function(){},
-        success:function(response) {
-            console.log(response);
-            if(response == 1){
+        data: $('#fomr_1').serialize(),
+        type: "POST",
+        url: 'controllers/UsuarioController.php?opcion=validar_usuario',
+        beforeSend: function() {},
+        success: function(response) {
+            //console.log(response);
+            if (response == 1) {
                 Swal.fire(
-                    'Exito!',
-                    'Bienvenido al Sistema <h1 style="color: lightblue">Aquialis</h1>',
-                    'success'
-                  );
-                  location.href = "views/inicio.php";
-            }else if(response == 2){
+                        'Exito!',
+                        'Bienvenido al Sistema <h1 style="color: lightblue">Aquialis</h1>',
+                        'success'
+                        );
+                location.href = "views/inicio.php";
+            } else if (response == 2) {
                 Swal.fire({
                     icon: 'error',
                     title: 'Oops...',
-                    text: 'Usuario no encontrado',    
+                    text: 'Usuario no encontrado',
                 });
                 LimpiarCampos();
             }
@@ -26,8 +26,7 @@ function ValidarUsuario() {
     });
 }
 
-function LimpiarCampos(){
+function LimpiarCampos() {
     $('#Correo').val("");
-    
     $('#Password').val("");
 }
